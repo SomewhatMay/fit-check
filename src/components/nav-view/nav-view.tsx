4627265ff7c5288f3ec7c5f4cff48746b932831e
SomewhatMay <umayeer.ahsan@hotmail.com>
2024-05-18 17:49:17 -0400

    prepared for deploy to iphone11

--- src/components/nav-view/nav-view.tsx
+++ src/components/nav-view/nav-view.tsx
@@ -10,7 +10,7 @@ interface props {
 
 export function NavView({ children }: props) {
   const [pageIndex, setPageIndex] = React.useState(
-    1 // Math.floor(children.length / 2)
+    Math.floor(children.length / 2)
   );
 
   return (
