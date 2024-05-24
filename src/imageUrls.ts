ca60b3fa0f6cdb976545d24178f2737a4efaa1c4
SomewhatMay <umayeer.ahsan@hotmail.com>
2024-05-23 21:10:33 -0400

    updated images to imgbb hosted

--- src/imageUrls.ts
+++ src/imageUrls.ts
@@ -1,7 +1,7 @@
 export const carouselImages = [
-  `https://cdn.discordapp.com/attachments/1240396785038524536/1241945415306186813/width119.png?ex=664cb40d&is=664b628d&hm=eb95bd0257c845a95b5c5503c0dd93df2d68095d7409ad9b14ab4603140ffc20&`,
-  `https://cdn.discordapp.com/attachments/1240396785038524536/1241945606465917039/width114.png?ex=664cb43a&is=664b62ba&hm=e09d875541ef0de699e4b0d9c443baaad7efa19d3adccf071ac179dbaa6a6648&`,
-  `https://cdn.discordapp.com/attachments/1240396785038524536/1241945691190984796/width118.png?ex=664cb44f&is=664b62cf&hm=ee62406a269c806627cef912397ae3574f0f9058f79cf7b051fffdaf5f300cde&`,
+  `https://i.ibb.co/XjJCzpv/fit2.webp`,
+  `https://i.ibb.co/b5RGHVc/fit3.webp`,
+  `https://i.ibb.co/JsRRPrj/fit1.webp`,
 ];
 
-export const profileIcon = `https://cdn.discordapp.com/attachments/1240396785038524536/1241945896409890836/width200.png?ex=664cb47f&is=664b62ff&hm=0e6f17363be9a4d2348b9bf016abf3ef2b007e3e30fe94c760a1a6f2be4ef020&`;
+export const profileIcon = `https://i.ibb.co/dbBbHR8/pfp.webp`;
